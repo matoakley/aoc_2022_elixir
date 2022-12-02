09a1c8875b086f28b949ce52e0e0daa6ed675ec0
Matt Oakley <matt.oakley@b4bpayments.com>
2022-12-02 16:30:16 +0000

    Day 2 Part 2

--- 2_the_elves_play_rock_paper_scissors/lib/rock_paper_scissors.ex
+++ 2_the_elves_play_rock_paper_scissors/lib/rock_paper_scissors.ex
@@ -1,26 +1,45 @@
 defmodule RockPaperScissors do
-  def calculate_total_score(input) do
+  def total_score(input), do: calculate_total_score(input)
+  def total_score_with_strategy_guide(input), do: calculate_total_score(input, true)
+
+  defp calculate_total_score(input, with_strategy_guide \\ false) do
     input
     |> String.trim
     |> String.split("\n")
-    |> Enum.map(fn round -> String.split(round) end)
-    |> Enum.reduce(0, fn [their_shape, your_shape], acc ->
-      [acc, shape_score(your_shape), round_score(their_shape, your_shape)]
-      |> Enum.sum
-    end)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(&maybe_implement_strategy_guide(&1, with_strategy_guide))
+    |> Enum.reduce(0, &sum_round_scores/2)
   end
 
-  defp shape_score("X"), do: 1
-  defp shape_score("Y"), do: 2
-  defp shape_score("Z"), do: 3
+  defp sum_round_scores([their_shape, your_shape], acc) do
+    acc
+    |> Kernel.+(shape_score(your_shape))
+    |> Kernel.+(round_score(their_shape, your_shape))
+  end
 
-  defp round_score("A", "Y"), do: 6
-  defp round_score("B", "Z"), do: 6
-  defp round_score("C", "X"), do: 6
+  defp maybe_implement_strategy_guide(input, false), do: input
+  defp maybe_implement_strategy_guide([their_shape, desired_result], true) do
+    [their_shape]
+    |> List.insert_at(1, shape_for_result(their_shape, desired_result))
+  end
 
-  defp round_score("A", "X"), do: 3
-  defp round_score("B", "Y"), do: 3
-  defp round_score("C", "Z"), do: 3
+  defp shape_for_result("A", "X"), do: "C"
+  defp shape_for_result("A", "Z"), do: "B"
+  defp shape_for_result("B", "X"), do: "A"
+  defp shape_for_result("B", "Z"), do: "C"
+  defp shape_for_result("C", "X"), do: "B"
+  defp shape_for_result("C", "Z"), do: "A"
+  defp shape_for_result(their_shape, "Y"), do: their_shape
 
+  defp shape_score(shape) when shape in ["A", "X"], do: 1
+  defp shape_score(shape) when shape in ["B", "Y"], do: 2
+  defp shape_score(shape) when shape in ["C", "Z"], do: 3
+
+  defp round_score("A", your_shape) when your_shape in ["B", "Y"], do: 6
+  defp round_score("B", your_shape) when your_shape in ["C", "Z"], do: 6
+  defp round_score("C", your_shape) when your_shape in ["A", "X"], do: 6
+  defp round_score("A", your_shape) when your_shape in ["A", "X"], do: 3
+  defp round_score("B", your_shape) when your_shape in ["B", "Y"], do: 3
+  defp round_score("C", your_shape) when your_shape in ["C", "Z"], do: 3
   defp round_score(_,_), do: 0
 end
--- 2_the_elves_play_rock_paper_scissors/test/rock_paper_scissors_test.exs
+++ 2_the_elves_play_rock_paper_scissors/test/rock_paper_scissors_test.exs
@@ -1,7 +1,7 @@
 defmodule RockPaperScissorsTest do
   use ExUnit.Case
 
-  describe "RockPaperScissors.calculate_total_score/1" do
+  describe "RockPaperScissors.total_score/1" do
     test "returns the total score based on shape played and round result" do
       input = """
       A Y
@@ -9,7 +9,19 @@ defmodule RockPaperScissorsTest do
       C Z
       """
 
-      assert RockPaperScissors.calculate_total_score(input) == 15
+      assert RockPaperScissors.total_score(input) == 15
+    end
+  end
+
+  describe "RockPaperScissors.total_score_with_strategy_guide/1" do
+    test "returns the total score based on shape played and round result" do
+      input = """
+      A Y
+      B X
+      C Z
+      """
+
+      assert RockPaperScissors.total_score_with_strategy_guide(input) == 12
     end
   end
 end
