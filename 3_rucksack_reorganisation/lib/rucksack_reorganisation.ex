862da2a3f3aec6d468468b56c322b2b1f436ad23
Matt Oakley <matt.oakley@b4bpayments.com>
2022-12-03 23:14:01 +0000

    Day 3 Part 2

--- 3_rucksack_reorganisation/lib/rucksack_reorganisation.ex
+++ 3_rucksack_reorganisation/lib/rucksack_reorganisation.ex
@@ -1,30 +1,40 @@
 defmodule RucksackReorganisation do
   def sum_priority_of_misplaced_types(input) do
     input
-    |> String.trim
-    |> String.split("\n")
+    |> String.split("\n", trim: true)
     # split into compartments
     |> Enum.map(fn items ->
-      compartment_size = String.length(items) |> Kernel.div(2)
-
-      {
-        String.slice(items, 0..compartment_size - 1),
-        String.slice(items, (compartment_size)..-1)
-      }
+      items |> String.to_charlist() |> Enum.split(div(String.length(items), 2))
     end)
-    # find the common letter
-    |> Enum.map(fn { first_compartment, second_compartment } = _bag_contents ->
+    |> Enum.map(fn { first_compartment, second_compartment } ->
       first_compartment
-      |> String.graphemes()
-      |> Enum.find(fn char -> String.contains?(second_compartment, char) end)
+      |> Enum.find(fn char -> Enum.find(second_compartment, &(&1==char)) end)
     end)
-    # calculate priority
+    |> calculate_priority()
+    |> Enum.sum
+  end
+
+  def sum_priority_of_group_badges(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn group -> Enum.map(group, &String.to_charlist/1) end)
+    # find the common letter
+    |> Enum.map(fn [first_elf, second_elf, third_elf] ->
+      first_elf
+      |> Enum.filter(fn char -> Enum.find(second_elf, &(&1==char)) end)
+      |> Enum.find(fn char -> Enum.find(third_elf, &(&1==char)) end)
+    end)
+    |> calculate_priority()
+    |> Enum.sum()
+  end
+
+  defp calculate_priority(chars) do
+    chars
     |> Enum.map(fn char ->
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      |> String.graphemes()
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
       |> Enum.find_index(&(&1==char))
       |> Kernel.+(1)
     end)
-    |> Enum.sum()
   end
 end
--- 3_rucksack_reorganisation/test/rucksack_reorganisation_test.exs
+++ 3_rucksack_reorganisation/test/rucksack_reorganisation_test.exs
@@ -1,7 +1,7 @@
 defmodule RucksackReorganisationTest do
   use ExUnit.Case
 
-  describe "RucksackReorganisation.sum_priority_misplaced_types/1" do
+  describe "RucksackReorganisation.sum_priority_of_misplaced_types/1" do
     test "returns an integer value" do
       input = """
       vJrwpWtwJgWrhcsFMMfFFhFp
@@ -13,6 +13,21 @@ defmodule RucksackReorganisationTest do
       """
 
       assert RucksackReorganisation.sum_priority_of_misplaced_types(input) == 157
+    end
+  end
+
+  describe "RucksackReorganisation.sum_priority_of_group_badges/1" do
+    test "returns an integer value" do
+      input = """
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
+      """
+
+      assert RucksackReorganisation.sum_priority_of_group_badges(input) == 70
     end
   end
 end
