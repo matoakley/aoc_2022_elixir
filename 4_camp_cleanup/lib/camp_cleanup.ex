d100f358a0336ed5003610faed141e7bcf8d76e6
Matt Oakley <matt.oakley@b4bpayments.com>
2022-12-04 11:02:53 +0000

    Day 4 Part 2

--- 4_camp_cleanup/lib/camp_cleanup.ex
+++ 4_camp_cleanup/lib/camp_cleanup.ex
@@ -1,5 +1,23 @@
 defmodule CampCleanup do
+  def containing_section_count(input) do
+    input
+    |> range_pairs_as_mapsets()
+    |> Enum.count(fn [first_section, second_section] ->
+      MapSet.subset?(first_section, second_section) ||
+        MapSet.subset?(second_section, first_section)
+    end)
+  end
+
   def overlapping_section_count(input) do
+    input
+    |> range_pairs_as_mapsets()
+    |> Enum.count(fn [first_section, second_section] ->
+      MapSet.intersection(first_section, second_section)
+      |> Enum.any?
+    end)
+  end
+
+  defp range_pairs_as_mapsets(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1, ","))
@@ -8,10 +26,6 @@ defmodule CampCleanup do
         MapSet.new(section_label_to_range(first_section)),
         MapSet.new(section_label_to_range(second_section))
       ]
-    end)
-    |> Enum.count(fn [first_section, second_section] ->
-      MapSet.subset?(first_section, second_section) ||
-        MapSet.subset?(second_section, first_section)
     end)
   end
 
--- 4_camp_cleanup/test/camp_cleanup_test.exs
+++ 4_camp_cleanup/test/camp_cleanup_test.exs
@@ -1,7 +1,7 @@
 defmodule CampCleanupTest do
   use ExUnit.Case
 
-  describe "CampCleanup.overlapping_section_count/1" do
+  describe "CampCleanup.containing_section_count/1" do
     test "returns a count of pairs where one is fully contained in the other" do
       input = """
       2-4,6-8
@@ -11,7 +11,21 @@ defmodule CampCleanupTest do
       6-6,4-6
       2-6,4-8
       """
-      assert CampCleanup.overlapping_section_count(input) == 2
+      assert CampCleanup.containing_section_count(input) == 2
+    end
+  end
+
+  describe "CampCleanup.overlapping_section_count/1" do
+    test "returns a count of pairs where there is overlap" do
+      input = """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+      """
+      assert CampCleanup.overlapping_section_count(input) == 4
     end
   end
 end
